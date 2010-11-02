8fe61cad8ba254ea9dbc10ce68916a1ee6115f61
andrej.panov <andrej.panov@363e8cb5-301e-3d41-34aa-1f7639939e5b>
2010-11-02 00:52:17 +0000

    added script for cleaning empty kern pairs

---
+++ clean_empty_kerns.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2010 Andrey V. Panov
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see "http://www.gnu.org/licenses/".
+#
+#   This fontforge script removes empty (i.e. zero) kerning pairs from font
+
+import fontforge
+import sys, getopt
+
+def usage():
+  print " -i file, --input=file     input font file"
+  print " -o file, --output=file    output font file"
+  print " -s , --subtable           kerning subtable"
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "hi:s:o:", ["help", "input=", "subtable=", "output="])
+except getopt.GetoptError, err:
+  print "unrecognized option"
+  usage()
+  sys.exit(2)
+outfile = None
+font_name = None
+kern_table = "LGC kerning subtable"
+kern_look = "LGC kerning"
+for (o, a) in opts:
+  if o in ("-i", "--input"):
+    font_name = a
+  elif o in ("-h", "--help"):
+    usage()
+    sys.exit()
+  elif o in ("-o", "--output"):
+    outfile = a
+  elif o in ("-s", "--subtable"):
+    kern_table = a
+
+if not outfile:
+  outfile = font_name
+#font_name = sys.argv[1]
+font = fontforge.open(font_name)
+font = fontforge.activeFont()
+font.selection.all()
+selection = font.selection.byGlyphs
+for glyf in selection:
+  if glyf.isWorthOutputting:
+    oldtable = glyf.getPosSub(kern_table)
+    if len(oldtable):
+      #print oldtable
+      newtable = ()
+      changekern = 0
+      for subtable in oldtable:
+        if subtable[1] == 'Pair':
+          if subtable[5] != 0:
+	    newtable += subtable,
+	  elif subtable[5] == 0:
+	    changekern = 1
+      if changekern:
+        glyf.removePosSub(kern_table)
+        for subtable in newtable:
+          #print subtable
+          glyf.addPosSub(subtable[0],subtable[2],subtable[3],\
+          subtable[4],subtable[5],subtable[6],subtable[7],\
+          subtable[8],subtable[9],subtable[10])
+
+font.save(outfile)
